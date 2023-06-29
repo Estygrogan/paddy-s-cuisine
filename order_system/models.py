784ca04e21d6baa33ba69de96de11e85b0c348d9
Estygrogan <dbobbet@gmail.com>
2023-06-29 16:39:24 +0000

    Add booking model

--- order_system/models.py
+++ order_system/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# from cloudinary.models import CloudinaryField
+
+# Booking Model
+
+class booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    time = models.TimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.title
